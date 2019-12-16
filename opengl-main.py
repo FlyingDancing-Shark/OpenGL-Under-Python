bab921069b30f17521eb6aa5f4cc5a0413ada389
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-16 16:22:32 +0800

    Update opengl-main.py

--- opengl-main.py
+++ opengl-main.py
@@ -1,4 +1,9 @@
 '''
 *********description*********
 '''
+import opengl-window-class
+from OpenGL.GL import *
+import numpy as np
+
+my_PyGL_window = opengl-window-class.Window(1280, 720, "My PyOpenGL window")
 
