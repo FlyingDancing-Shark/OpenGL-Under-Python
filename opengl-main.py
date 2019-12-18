bc6ed1a728f3037c88c21214a6a1b3f8c63a4b91
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-18 14:27:24 +0800

    Update opengl-main.py

--- opengl-main.py
+++ opengl-main.py
@@ -3,7 +3,9 @@
 the main opengl application using shader ( programmable pipeline) with shader source string hard-coded inside.
 '''
 import opengl-window-class
-
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+import numpy as np
 
 
 vertex_src = """
@@ -28,5 +30,25 @@ void main()
 
 my_PyGL_window = opengl-window-class.Window(1280, 720, "My PyOpenGL window")
 
+vertices = [-0.5, -0.5, 0.0,
+	     0.5, -0.5, 0.0,
+	     0.0,  0.5, 0.0]
 
+colors = [1.0, 0.0, 0.0,
+	  0.0, 1.0, 0.0,
+	  0.0, 0.0, 1.0]
+
+np_vertices = np.array(vertices, dtype=np.float32)
+np_colors   = np.array(colors  , dtype=np.float32)
+
+# this internally use glCompileShader() and all the other necessary works to link a 
+# shader program, decrease the coding complexity for programmers. 
+shader_program_handle = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), \
+				       compileShader(fragment_src, GL_FRAGMENT_SHADER))
+
+glClearColor(0, 0.1, 0.1, 1)
+
+
+# after an opengl context are created and all related drawing configuration complete, 
+# we enter a loop probe for any user mouse click and key strike.
 my_PyGL_window.main_loop()
